63f9e3bfd95e6cce6f50f23a7685a48bd7347325
afeiship <1290657123@qq.com>
2023-08-14 10:27:51 +0800

    feat: init vue-envs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
+import EnvManager from '@jswork/env-manager';
+
 declare var wx: any;
 
-const VueEnvs = (): void => {
-  console.log('hello');
-};
+const vueEnv = new EnvManager({
+  prefix: 'VUE_APP_',
+  env: process.env,
+});
+
+class VueEnvs {
+  public static get(inKey?: string): any {
+    return vueEnv.get(inKey);
+  }
+
+  public static set(inCmdRc: any): void {
+    return vueEnv.set(inCmdRc);
+  }
+}
 
 // for commonjs es5 require
 if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
